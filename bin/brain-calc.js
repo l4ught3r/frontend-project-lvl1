fdc575288f514924bdca068d6d10a271600a155e
Anton Kuchin <l4ught3r@mail.ru>
2022-08-07 01:12:08 +0700

    fix import

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/games/cacl-game.js';
+import { startGame } from '../src/games/calc-game.js';
 
 startGame();
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/games/prime-game.js';
+import { startGame } from '../src/games/prime-game.js';
 
 startGame();
