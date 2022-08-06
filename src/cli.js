26701fb83488cc14e2e368169f6b78783b71e3fd
Anton Kuchin <l4ught3r@mail.ru>
2022-08-07 00:58:09 +0700

    cli.js fix

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  return (`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
 };
 
 export const foo = () => 0;
